140c5b24740710366479775275842d9d6ebe2891
Farhan Jailani <farhanjailani@gmail.com>
2017-08-23 23:54:05 +0530

    Add files via upload

--- Contact.java
+++ Contact.java
@@ -1,17 +1,26 @@
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="Contacts_Table")
 public class Contact {
 	@Id
+	@Column(name="ID")
 	private Integer id;
+	@Column(name="First_Name")
 	private String firstName;
+	@Column(name="Last_Name")
 	private String lastName;
+	@Column(name="EmailID")
 	private String emailId;
+	@Column(name="PhoneNo")
 	private Long phoneNo;
+	@Column(name="DOB")
 	private Date dateOfBirth;
 
 	public Contact() {
@@ -56,6 +65,14 @@ public class Contact {
 
 	public void setPhoneNo(Long phoneNo) {
 		this.phoneNo = phoneNo;
+	}
+
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
 	}
 
 
